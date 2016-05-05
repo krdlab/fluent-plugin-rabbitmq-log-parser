7299ace45417f7b566f43395b9a716eb5edadfe6
krdlab <krdlab@gmail.com>
2016-05-05 18:54:41 +0900

    Create project (bundle gem)

---
+++ lib/fluent/plugin/rabbitmq/log/parser.rb
@@ -0,0 +1,13 @@
+require "fluent/plugin/rabbitmq/log/parser/version"
+
+module Fluent
+  module Plugin
+    module Rabbitmq
+      module Log
+        module Parser
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/rabbitmq/log/parser/version.rb
@@ -0,0 +1,11 @@
+module Fluent
+  module Plugin
+    module Rabbitmq
+      module Log
+        module Parser
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
